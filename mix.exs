22ba9ac65104b4a97e2aab74663fab3850ac27db
Marcelo Belli <marcelo@belli.me>
2018-07-13 23:25:02 -0300

    Project dependecies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,11 @@ defmodule VagabondSqs.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_sqs, "~> 2.0"},
+      {:hackney, "~> 1.9"},
+      {:poison, "~> 3.1"},
+      {:sweet_xml, "~> 0.6"}
     ]
   end
 end
